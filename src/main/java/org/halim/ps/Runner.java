59ad8af0563e026e9d2161642da58748436d1e51
ahalim7 <ahalim7@yahoo.com>
2021-06-27 12:01:45 +0200

    Validate Images

--- src/main/java/org/halim/ps/Runner.java
+++ src/main/java/org/halim/ps/Runner.java
@@ -4,6 +4,7 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.poi.openxml4j.exceptions.NotOfficeXmlFileException;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
+import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,6 +15,8 @@ public class Runner {
     public static void main(String[] args) {
         validateWord();
         validatePDF();
+        validateImage("file.png");
+        validateImage("file.jpg");
     }
 
     private static void validateWord() {
@@ -30,6 +33,18 @@ public class Runner {
             PDDocument.load(new File("file.pdf"));
         } catch (IOException e) {
             //TODO:: invalid pdf file, write your logic here
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void validateImage(String file) {
+        try {
+            if (ImageIO.read(new File(file)) == null)
+                //TODO:: invalid image file, write your logic here
+                System.out.println("Empty Content");
+
+        } catch (IOException e) {
+            //TODO:: invalid image file, write your logic here
             System.out.println(e.getMessage());
         }
     }
