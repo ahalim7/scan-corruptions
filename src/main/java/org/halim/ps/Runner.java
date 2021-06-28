1f353c61f348fcd0622ce38550577d43438316ce
ahalim7 <ahalim7@yahoo.com>
2021-06-28 09:27:04 +0200

    cleanup

--- src/main/java/org/halim/ps/Runner.java
+++ src/main/java/org/halim/ps/Runner.java
@@ -13,39 +13,50 @@ import java.nio.file.Paths;
 public class Runner {
 
     public static void main(String[] args) {
-        validateWord();
-        validatePDF();
-        validateImage("file.png");
-        validateImage("file.jpg");
+        if (isValidOfficeDocument())
+            System.out.println("write your logic in this block");
+
+        if (isValidPDF())
+            System.out.println("write your logic in this block");
+
+        if (isValidImage("file.png"))
+            System.out.println("write your logic in this block");
+
+        if (isValidImage("file.jpg"))
+            System.out.println("write your logic in this block");
+
     }
 
-    private static void validateWord() {
+    private static boolean isValidOfficeDocument() {
         try {
             new XWPFDocument(Files.newInputStream(Paths.get("file.doc")));
+            return Boolean.TRUE;
         } catch (NotOfficeXmlFileException | IOException e) {
             //TODO:: invalid office file, write your logic here
-            System.out.println(e.getMessage());
+            return Boolean.FALSE;
         }
     }
 
-    private static void validatePDF() {
+    private static boolean isValidPDF() {
         try {
             PDDocument.load(new File("file.pdf"));
+            return Boolean.TRUE;
         } catch (IOException e) {
             //TODO:: invalid pdf file, write your logic here
-            System.out.println(e.getMessage());
+            return Boolean.FALSE;
         }
     }
 
-    private static void validateImage(String file) {
+    private static boolean isValidImage(String file) {
         try {
             if (ImageIO.read(new File(file)) == null)
                 //TODO:: invalid image file, write your logic here
-                System.out.println("Empty Content");
+                return Boolean.FALSE;
 
+            return Boolean.TRUE;
         } catch (IOException e) {
             //TODO:: invalid image file, write your logic here
-            System.out.println(e.getMessage());
+            return Boolean.FALSE;
         }
     }
 
